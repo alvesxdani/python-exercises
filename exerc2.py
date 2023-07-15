d77fdd0adbe0b27c242b38a6f77662283e2a0fa3
alvesxdani <dani.alvesm22@gmail.com>
2023-07-15 10:53:58 -0300

    Add new functions

--- exerc2.py
+++ exerc2.py
@@ -1,20 +1,16 @@
 # MELHORANDO O CÓDIGO E FUNÇÕES
-
-saldo = float(0)
-EXTRATO_OPS = []
-TOTAL_SAQUES = 0
-LIMITE_SAQUES = 3
-AGENCIA = "0001"
-
-operacao = int(input("""
-======== MENU ========
-Qual operação será realizada?
-[1] Saque
-[2] Depósito
-[3] Extrato
-[4] Cadastrar novo usuário
-[5] Sair
-"""))
+def menu():
+    menu = int(input("""
+    ======== MENU ========
+    Qual operação será realizada?
+    [1] Saque
+    [2] Depósito
+    [3] Extrato
+    [4] Cadastrar novo usuário
+    [5] Filtrar usuários
+    [0] Sair
+    """))
+    return menu
 
 # Functions
 def saque(valor):
@@ -48,23 +44,61 @@ def extrato():
     print("\n ##### SEU EXTRATO #####")
     print(f"Seu saldo é de: {saldo : .2f}")
 
+def criar_usuario(usuarios):
+   cpf = int(input('Informe o CPF (apenas números): '))
+   usuario = filtrar_usuarios(cpf, usuarios)
+
+   if usuario:
+      print('\n Já existe um usuário com este CPF!')
+
+   nome = input('Informe o nome completo: ')
+   data_nascimento = input('Informe a data de nascimento (dd-mm-aaaa): ')
+  
+   usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf})
+
+def filtrar_usuarios(cpf, usuarios):
+   usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+   return usuarios_filtrados[0] if usuarios_filtrados else None
+   
+
+
+
 # program
-while operacao != 5:
-    if operacao == 1 and TOTAL_SAQUES < LIMITE_SAQUES:
-     valor = float(input("Quantidade a sacar: "))
-     saque(valor)
+def main():
+   saldo = float(0)
+   EXTRATO_OPS = []
+   TOTAL_SAQUES = 0
+   LIMITE_SAQUES = 3
+   USUARIOS = []
+   CONTAS = []
+
+   operacao = menu()
+   
+   while operacao != 0:
+     if operacao == 1 and TOTAL_SAQUES < LIMITE_SAQUES:
+        valor = float(input("Quantidade a sacar: "))
+        saque(valor)
     
-    elif operacao == 0 and TOTAL_SAQUES <= LIMITE_SAQUES:
+     elif operacao == 0 and TOTAL_SAQUES <= LIMITE_SAQUES:
         print('Você atingiu o limite de saques diários (3 saques por dia)')
 
-    elif operacao == 2:
+     elif operacao == 2:
         valor = float(input("Quantidade a depositar: "))
         deposito(valor)
-    
-    elif operacao == 3:
+     
+     elif operacao == 3:
         extrato()
 
-    else:
-        operacao = int(input("Operação desconhecida. Digite novamente: "))
+     elif operacao == 4:
+        criar_usuario(USUARIOS)
 
-    operacao = int(input("Digite o número da nova operação: "))
+     elif operacao == 5:
+        cpf = input('Digite o CPF: ')
+        filtrar_usuarios(cpf, USUARIOS)
+     
+     else:
+        operacao = int(input("Operação desconhecida. Digite novamente: "))
+     
+     operacao = int(input("Digite o número da nova operação: "))
+
+main()
